c585c9265d228ad06bf8b3732abaf9a9619c0f27
Hossein Pira <h3dev.pira@gmail.com>
2025-03-09 08:36:03 +0330

    add api & add api-docs & add size selector & add auto translate prompts to en & fix bugs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,29 +1,37 @@
-import { MetadataRoute } from 'next';
+import { NextApiResponse } from 'next';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export async function GET() {
   const baseUrl = 'https://cinemaplus-app.vercel.app';
   const lastMod = new Date().toISOString();
 
-  return [
-    {
-      url: `${baseUrl}/`,
-      lastModified: lastMod,
-      priority: 1.00
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<!--  created with Free Online Sitemap Generator www.xml-sitemaps.com  -->
+<url>
+<loc>${baseUrl}/</loc>
+<lastmod>${lastMod}</lastmod>
+<priority>1.00</priority>
+</url>
+<url>
+<loc>${baseUrl}/series</loc>
+<lastmod>${lastMod}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/search</loc>
+<lastmod>${lastMod}</lastmod>
+<priority>0.80</priority>
+</url>
+<url>
+<loc>${baseUrl}/settings</loc>
+<lastmod>${lastMod}</lastmod>
+<priority>0.80</priority>
+</url>
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml',
     },
-    {
-      url: `${baseUrl}/series`,
-      lastModified: lastMod,
-      priority: 0.80
-    },
-    {
-      url: `${baseUrl}/search`,
-      lastModified: lastMod,
-      priority: 0.80
-    },
-    {
-      url: `${baseUrl}/settings`,
-      lastModified: lastMod,
-      priority: 0.80
-    }
-  ];
+  });
 } 
