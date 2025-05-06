7380ab776d92890b6fa84c78d8a87ddcb8774371
MamdMehrabi <mohammadmehrabi175@gmail.com>
2025-05-06 16:42:16 +0330

    add meta tag to tv

--- app/tv/layout.tsx
+++ app/tv/layout.tsx
@@ -6,10 +6,19 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'تلویزیون زنده | Cinema Plus | سینما پلاس',
     description: 'Watch live TV channels from around Iran | تماشای کانال‌های تلویزیونی زنده از سراسر ایران',
+    images: [
+      {
+        url: '/tv.png',
+        width: 1200,
+        height: 630,
+        alt: 'Cinema Plus - Modern Movie Application | سینما پلاس - اپلیکیشن مدرن فیلم و سریال',
+      },
+    ],
   },
   twitter: {
     title: 'تلویزیون زنده | Cinema Plus | سینما پلاس',
     description: 'Watch live TV channels from around Iran | تماشای کانال‌های تلویزیونی زنده از سراسر ایران',
+    images: ["/tv.png"]
   },
 };
 
