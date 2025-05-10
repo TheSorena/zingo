f2a3ffcdc598be5c8afd1d259e6ee4d7b1461d3e
Hossein Pira <h3dev.pira@gmail.com>
2025-05-11 00:27:01 +0330

    update

--- app/page.tsx
+++ app/page.tsx
@@ -167,7 +167,7 @@ export default async function Home() {
           <div className="md:hidden flex items-center">
             <Link href="/search">
               <Button variant="ghost" size="icon" aria-label="Search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
               </Button>
             </Link>
           </div>
@@ -206,6 +206,8 @@ export default async function Home() {
           ) : (
             <ErrorState />
           )}
+
+          <hr />
 
           {topRatedMovies ? (
             <MovieSlider title="30 سینمایی برتر (بر اساس IMDB)" movies={topRatedMovies} />
